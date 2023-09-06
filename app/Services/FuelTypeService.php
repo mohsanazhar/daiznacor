0a5ecca56113931ae0bd03dd5020916346c85e65
Ahsan Azhar <dawlance64@gmail.com>
2023-09-06 11:48:43 +0500

    August 31 fixes
    Signed-off-by: Ahsan Azhar <dawlance64@gmail.com>

--- app/Services/FuelTypeService.php
+++ app/Services/FuelTypeService.php
@@ -27,7 +27,7 @@ class FuelTypeService
     }   
 
     public function get(){
-        return FuelType::orderByAsc("id")->get()->toArray();
+        return FuelType::get()->toArray();
     }
 
     public function create($name){
